d4da1cdaf53d682488d67b002032bef49fd205fd
Daweenci <we3bk1ng@gmail.com>
2026-04-15 16:12:23 +0200

    add meteor rotation

--- meteor.gd
+++ meteor.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
 var speed = 300
+var rotation_speed = randf_range(-5.0, 5.0)
 
 func _ready():
 	add_to_group("obstacle")
+	rotation = randf_range(0, TAU)
 
 func _process(delta):
 	position.y += speed * delta
+	rotation += rotation_speed * delta
 
 	if position.y > 800:
 		queue_free()
